e04f8d8822491535dc25b1f781cf292032deb3ff
Nguyen Dinh Quang <20010818@st.phenikaa-uni.edu.vn>
2024-07-14 17:42:03 +0700

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const UserRouter = require('./UserRouter')
 const ProductRouter = require('./productRouter')
 const OrderRouter = require('./OrderRouter')
 const PostRouter = require('./PostRouter')
+// const FavoriteRouter = require('./FavoriteRouter')
 const LocationRouter = require('./LocationRouter')
 const routes = (app) => {
     app.use('/api/user', UserRouter)
@@ -9,6 +10,8 @@ const routes = (app) => {
     app.use('/api/order', OrderRouter)
     app.use('/api/post', PostRouter)
     app.use('/api',LocationRouter)
+    // app.use('/api/favorite',FavoriteRouter)
+
 
 
 }
